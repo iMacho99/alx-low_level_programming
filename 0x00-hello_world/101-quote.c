9efb8b4fb548e221cb6b397cf21009d0a570e637
Macham <Machambaak100@gmail.com>
2023-03-16 18:17:57 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,15 @@
+#include <stdio.h>
 #include <unistd.h>
 
-
 /**
- * Main - function
- *
- * Return: 1
+ * main - prints exactly and that piece of art is useful
+ * followed by a new line, to the standard error.
+ * Return: Always zero (Sucess)
  */
-
 
 int main(void)
 {
-
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	const size_t len = sizeof(msg) - 1;
-	ssize_t ret = write(STDERR_FILENO, msg, len);
-	return (ret = len); 1;
-
+	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
