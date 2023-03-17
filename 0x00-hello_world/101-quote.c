17981c736b3bd6dfd947b8b957bed0df84955bb5
Macham <Machambaak100@gmail.com>
2023-03-16 17:58:03 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,19 @@
 #include <unistd.h>
 
+
 /**
  * Main - function
  *
  * Return: 1
  */
 
+
 int main(void)
 {
+
 	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	const size_t len = sizeof(msg) - 1;
 	ssize_t ret = write(STDERR_FILENO, msg, len);
-	return (ret = len) ? 1 : 0;
+	return (ret = len); 1;
+
 }
