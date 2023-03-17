95f441760838e46587395859c44eac2aa81b845c
Macham <Machambaak100@gmail.com>
2023-03-16 19:43:32 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly and that piece of art is useful
+ * main - prints and that piece of art is useful
  * followed by a new line, to the standard error
- * Return: Always zero (Sucess)
+ * Return: Always 0 (Sucess)
  */
 
 int main(void)
-{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+{	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
+	return (1); }
